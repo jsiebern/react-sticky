a1934a4f608a3eae3b9f8863fa5fdce2517ed5b8
Jonathan Siebern <jsiebern@web.de>
2017-04-17 14:29:59 +0200

    Fix deprecated warning for react prop-types

--- src/sticky.js
+++ src/sticky.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 export default class Sticky extends React.Component {
 
   static propTypes = {
-    isActive: React.PropTypes.bool,
-    className: React.PropTypes.string,
-    style: React.PropTypes.object,
-    stickyClassName: React.PropTypes.string,
-    stickyStyle: React.PropTypes.object,
-    topOffset: React.PropTypes.number,
-    bottomOffset: React.PropTypes.number,
-    onStickyStateChange: React.PropTypes.func,
-    showPlaceholder: React.PropTypes.bool
+    isActive: PropTypes.bool,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    stickyClassName: PropTypes.string,
+    stickyStyle: PropTypes.object,
+    topOffset: PropTypes.number,
+    bottomOffset: PropTypes.number,
+    onStickyStateChange: PropTypes.func,
+    showPlaceholder: PropTypes.bool
   }
 
   static defaultProps = {
@@ -28,7 +29,7 @@ export default class Sticky extends React.Component {
   }
 
   static contextTypes = {
-    'sticky-channel': React.PropTypes.any
+    'sticky-channel': PropTypes.any
   }
 
   constructor(props) {
